9b0dfcdd8bb6fe24580cfa294a7f9b38dcaeb397
Andrey <dayl1ex@gmail.com>
2015-12-20 14:38:03 +0300

    fixed 1.3

--- python/lab1.3.py
+++ python/lab1.3.py
@@ -7,8 +7,8 @@ import urlparse
 
 
 def FindNextUrlWithWord(url, word):
-    if not (url in checkedPages):
-        checkedPages.append(url)
+    if not checkedPages.has_key(url):
+        checkedPages[url] = 1
         content = urllib2.urlopen(url).read()
 
         #finding all links and making them absolute
@@ -17,6 +17,7 @@ def FindNextUrlWithWord(url, word):
 
         if word in content: #looking for word on page
             print 'Found new page with word'
+            checkedPages[url] = 1
             DownloadPage(url)
         for link in absoluteLinksList:
             #looking for word in new link if it's a webpage
@@ -38,8 +39,15 @@ def SwitchBackToPrevDir():
         filePath += folderParts[i] + '\\'
     os.chdir(filePath)
 
+def GetCorrectFileName(name):
+    invalidSymbols = ['/', '|','"','*',':','?', '<', '>','\\' ]
+    correctName = ''
+    for symbol in name:
+        if symbol not in invalidSymbols:
+            correctName += symbol
+    return correctName
+
 def DownloadPage(url):
-    checkedPages.append(url)
     page = urllib2.urlopen(url).read()
     print page
 
@@ -48,14 +56,16 @@ def DownloadPage(url):
     convertedFiles = [urlparse.urljoin(url, n) for n in links]
 
     #creating folder for pageFiles
-    folder =  re.findall('<title>(.*?)</title>', page)[0]
+    namePart =  re.findall('<title>(.*?)</title>', page)[0]
+    folder = GetCorrectFileName(namePart)
     os.mkdir(folder)
     os.chdir(folder)
 
     #downloading files to this folder
     for i in range(len(links)):
         url = links[i]
-        fileName = url.rsplit('/', 1)[-1]
+        namePart = url.rsplit('/', 1)[-1]
+        fileName = GetCorrectFileName(namePart)
         try:
             if '.' in fileName: #download file
                 urllib.urlretrieve(convertedFiles[i], fileName)
@@ -69,7 +79,6 @@ def DownloadPage(url):
 
 url = "https://python.org"
 word = 'programming'
-checkedPages = [] #list of already used urls
-
+checkedPages = {}
 FindNextUrlWithWord(url, word)
 print 'Program has finished.'
